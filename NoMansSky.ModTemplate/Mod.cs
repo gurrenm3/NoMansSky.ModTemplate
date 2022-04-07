5364167da6b0414dd3ab4f17fb46ac258eca5944
Thomas Margraff <thomaspmar@protonmail.com>
2022-04-06 20:27:00 -0700

    Add project files.

---
+++ NoMansSky.ModTemplate/Mod.cs
@@ -0,0 +1,37 @@
+﻿using Reloaded.Hooks.ReloadedII.Interfaces;
+using Reloaded.Mod.Interfaces;
+using NoMansSky.Api;
+
+namespace NoMansSky.ModTemplate
+{
+    /// <summary>
+    /// Your mod logic goes here.
+    /// </summary>
+    public class Mod : NMSMod
+    {
+        /// <summary>
+        /// Initializes your mod along with some necessary info.
+        /// </summary>
+        public Mod(Game _game, IModConfig _config, IReloadedHooks _hooks, ILogger _logger) : base(_game, _config, _hooks, _logger)
+        {
+            Logger.WriteLine("Hello World!");
+
+            // Below are 2 examples of using ModEvents
+            _game.OnProfileSelected += () => Logger.WriteLine("The player just selected a save file");
+            _game.OnMainMenu += OnMainMenu;
+        }
+
+        /// <summary>
+        /// Called once every frame.
+        /// </summary>
+        public override void Update()
+        {
+            
+        }
+
+        private void OnMainMenu()
+        {
+            Logger.WriteLine("Main Menu shown!");
+        }
+    }
+}
